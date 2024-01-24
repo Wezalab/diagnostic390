cd71fb1a843fe1115849e6dd6b7ea0097b777bb1
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-01-25 00:07:44 +0100

    Add list of prod

--- src/pages/Commande/DetailsCommande.js
+++ src/pages/Commande/DetailsCommande.js
@@ -1,18 +1,18 @@
 import { Helmet } from 'react-helmet-async';
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 // @mui
 import {
-  Container, Box, Typography, Link, Paper,
+  Container, Box, Typography, Link, Card, Divider,
 } from '@mui/material';
+import Grid from '@mui/material/Unstable_Grid2';
 
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Iconify from '../../components/iconify';
 
 
 export default function DetailsCommande() {
   const location = useLocation();
-  const navigate = useNavigate();
   const { commandeObject } = location.state || {};
 
   // eslint-disable-next-line no-unused-vars
@@ -39,9 +39,45 @@ export default function DetailsCommande() {
                 <Typography variant="h6">Commande # N°{commande.number}</Typography>
               </div>
             </Box>
-            <Typography sx={{marginLeft: 5, color: "#aaa"}} variant="caption">Le 25 janv 2924{commande.number}</Typography>
+            <Typography sx={{ marginLeft: 5, color: "#aaa" }} variant="caption">Le 25 janv 2924{commande.number}</Typography>
           </Box>
         </Box>
+
+        <Grid container spacing={4}>
+          <Grid item xs={8}>
+            <Card sx={{ padding: 4 }} >
+              <Typography variant="h6">Details</Typography>
+              <Box>
+                {
+                  commande.line_items.map((prod, key) => 
+                     <div key={key} ><Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between", alignItems: 'center' }}>
+                      <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                        <img style={{ width: 70, border: '1px solid #EEE', borderRadius: 5, cursor: 'pointer', margin:8  }} alt={prod?.image?.id} src={prod?.image?.src} />
+                        <Box>
+                          <Typography variant="subtitle2">{prod.name}</Typography>
+                          <Typography variant="caption">Le 25 janv 2924{commande.number}</Typography>
+                        </Box>
+                      </Box>
+
+                      <Typography variant="body2">X{prod.quantity}</Typography>
+                      <Typography variant="subtitle1">{prod.price} $</Typography>
+                    </Box>
+                      <Divider />
+                    </div>
+
+                  )
+                }
+              </Box>
+            </Card>
+
+          </Grid>
+          <Grid item xs={4}>
+            <Card>
+              <Typography variant="caption">Le 25 janv 2924{commande.number}</Typography>
+
+            </Card>
+          </Grid>
+        </Grid>
 
       </Container>
     </>
