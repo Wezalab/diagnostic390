34bb0502c2681160707311d5f4a2ef9efc600028
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-01-26 11:33:02 +0100

    Add BonDeCommand component

--- src/pages/Commande/DetailsCommande.js
+++ src/pages/Commande/DetailsCommande.js
@@ -17,6 +17,7 @@ import MenuList from '@mui/material/MenuList';
 
 import { useLocation } from 'react-router-dom';
 import Iconify from '../../components/iconify';
+import BonCommande from '../Facture/BonCommande';
 
 const options = ["En attente", "Traitement", "Terminé", "En pause", "Annulé", "Remboursé", "Echec et poubelle"];
 // const options2 = ['pending', 'processing', 'on-hold', 'completed', 'cancelled', 'refunded', 'failed and trash'];
@@ -142,44 +143,7 @@ export default function DetailsCommande() {
 
         <Grid container spacing={4}>
           <Grid item xs={8}>
-            <Card sx={{ padding: 4 }} >
-              <Typography variant="h6">Details</Typography>
-              <Box>
-                {
-                  commande.line_items.map((prod, key) =>
-                    <div key={key} >
-                      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between", alignItems: 'center' }}>
-                        <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                          <img style={{ width: 70, border: '1px solid #EEE', borderRadius: 5, cursor: 'pointer', margin: 8 }} alt={prod?.image?.id} src={prod?.image?.src} />
-                          <Box>
-                            <Typography variant="subtitle2">{prod.name}</Typography>
-                            <Typography variant="caption">{commande.date_created}</Typography>
-                          </Box>
-                        </Box>
-                        <Typography variant="body2">X{prod.quantity}</Typography>
-                        <Typography variant="subtitle1">{prod.price} $</Typography>
-                      </Box>
-
-                      <Divider />
-
-                    </div>
-                  )}
-                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between", marginTop: 2 }} >
-                  <Typography variant="subtitle1" sx={{ marginLeft: '70%' }}>Sous total : </Typography>
-                  <Typography variant="body1">{commande.total}$</Typography>
-                </Box>
-
-                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: 2 }} >
-                  <Typography sx={{ color: '#a21', marginLeft: '70%' }} variant="subtitle1">Taxe : </Typography>
-                  <Typography sx={{ color: '#a21' }} variant="body1">0</Typography>
-                </Box>
-
-                <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: 2 }} >
-                  <Typography sx={{ marginLeft: '70%' }} variant="subtitle1">TOTAL : </Typography>
-                  <Typography variant="subtitle1">{commande.total}</Typography>
-                </Box>
-              </Box>
-            </Card>
+            <BonCommande commande={commande} />
 
           </Grid>
           <Grid item xs={4}>
---
+++ src/pages/Facture/BonCommande.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import React, { useState } from "react";
+
+import { Box, Card, Divider, Typography } from '@mui/material';
+
+
+// BonCommande.propTypes = {
+//     commande: PropTypes.any,
+// };
+
+const BonCommande = ({commande}) => 
+
+     <Card sx={{ padding: 4 }} >
+    <Typography variant="h6">Details</Typography>
+    <Box>
+      {
+        commande.line_items.map((prod, key) =>
+          <div key={key} >
+            <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between", alignItems: 'center' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <img style={{ width: 70, border: '1px solid #EEE', borderRadius: 5, cursor: 'pointer', margin: 8 }} alt={prod?.image?.id} src={prod?.image?.src} />
+                <Box>
+                  <Typography variant="subtitle2">{prod.name}</Typography>
+                  <Typography variant="caption">{commande.date_created}</Typography>
+                </Box>
+              </Box>
+              <Typography variant="body2">X{prod.quantity}</Typography>
+              <Typography variant="subtitle1">{prod.price} $</Typography>
+            </Box>
+
+            <Divider />
+
+          </div>
+        )}
+      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between", marginTop: 2 }} >
+        <Typography variant="subtitle1" sx={{ marginLeft: '70%' }}>Sous total : </Typography>
+        <Typography variant="body1">{commande.total}$</Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: 2 }} >
+        <Typography sx={{ color: '#a21', marginLeft: '70%' }} variant="subtitle1">Taxe : </Typography>
+        <Typography sx={{ color: '#a21' }} variant="body1">0</Typography>
+      </Box>
+
+      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginTop: 2 }} >
+        <Typography sx={{ marginLeft: '70%' }} variant="subtitle1">TOTAL : </Typography>
+        <Typography variant="subtitle1">{commande.total}</Typography>
+      </Box>
+    </Box>
+  </Card>
+
+
+export default BonCommande;
