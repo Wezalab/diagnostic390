64b6f9a8661c0c666cad71c1dd7ed18e42c924b5
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-01-31 14:54:07 +0100

    Add store info

--- src/components/facture/BonCommandeHeader.js
+++ src/components/facture/BonCommandeHeader.js
@@ -6,23 +6,24 @@ import { Box, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 
 
-const BonCommandeHeader = ({ selectedUser}) =>
+const BonCommandeHeader = ({ selectedUser, commande}) =>
 
   <Box sx={{marginBottom: 2, display:'none', displayPrint:'block'}} >
     <Typography variant="h4">Bon de commande</Typography>
     <Grid container spacing={4} marginTop={3} marginBottom={2}>
       <Grid item xs={6} sx={{ display: 'flex', flexDirection: 'column' }} >
         <Typography variant="subtitle1">Emis par:</Typography>
-        <Typography variant="caption">Nom</Typography>
-        <Typography variant="caption">Address</Typography>
+        <Typography variant="caption">{selectedUser?.username} {selectedUser?.first_name}  {selectedUser?.last_name} </Typography>
+        <Typography variant="caption">{selectedUser?.billing?.address_1} {selectedUser?.billing?.address_2}</Typography>
         <Typography variant="caption">{selectedUser.email}</Typography>
-        <Typography variant="caption">telephone</Typography>
-        <Typography variant="caption">site web</Typography>
+        <Typography variant="caption">{selectedUser?.billing?.phone}</Typography>
+        {/* <Typography variant="caption">site web</Typography> */}
       </Grid>
       <Grid item xs={6} sx={{ display: 'flex', flexDirection: 'column' }} >
         <Typography variant="subtitle1">Destinateur:</Typography>
-        <Typography variant="caption">Nom</Typography>
-        <Typography variant="caption">Address</Typography>
+        <Typography variant="caption">{commande?.store?.shop_name} {commande?.store?.name}</Typography>
+        <Typography variant="caption">{commande?.store?.address?.street_1}, {commande?.store?.address?.street_2},
+        {commande?.store?.address?.city}, {commande?.store?.address?.country}</Typography>
         <Typography variant="caption">email</Typography>
         <Typography variant="caption">telephone</Typography>
         <Typography variant="caption">site web</Typography>
@@ -46,7 +47,7 @@ const BonCommandeHeader = ({ selectedUser}) =>
   </Box>
 
 BonCommandeHeader.propTypes = {
-  // commande: PropTypes.object,
+  commande: PropTypes.object,
   selectedUser: PropTypes.object,
 };
 
